5e8fe45f28cff86cefaf971ab0a0c19cdd2ba227
jsong00505 <jsong00505@gmail.com>
2017-03-01 15:09:22 +0900

    chore: fix indentation

--- 30-days-of-code/src/day0x/day01/datatypes/Solution.java
+++ 30-days-of-code/src/day0x/day01/datatypes/Solution.java
@@ -22,21 +22,21 @@ public class Solution {
 		double d = 4.0;
 		String s = "HackerRank ";
 
-	    /* Declare second integer, double, and String variables. */
+		/* Declare second integer, double, and String variables. */
 		int iPrime = Integer.parseInt(scan.next());
 		double dPrime = Double.parseDouble(scan.next());
 		scan.nextLine(); /* For new line! actually, I am not comfortable with that because it looks bad. */
 		String sPrime = scan.nextLine();
 
-	    /* Read and save an integer, double, and String to your variables.*/
+		/* Read and save an integer, double, and String to your variables.*/
 		// Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
 
-	    /* Print the sum of both integer variables on a new line. */
+		/* Print the sum of both integer variables on a new line. */
 		System.out.println(i+iPrime);
-	    /* Print the sum of the double variables on a new line. */
+		/* Print the sum of the double variables on a new line. */
 		System.out.println(d+dPrime);
-	    /* Concatenate and print the String variables on a new line;
-	    	the 's' variable above should be printed first. */
+		/* Concatenate and print the String variables on a new line;
+			the 's' variable above should be printed first. */
 		System.out.println(s+sPrime);
 
 		// close
