e03aff6189793bc2516647f87dc7c80b74751b4a
jsong00505 <jsong00505@gmail.com>
2017-03-05 21:38:37 +0900

    feat: Java Loops I

---
+++ java/src/introduction/loops1/Solution.java
@@ -0,0 +1,38 @@
+package introduction.loops1;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Created by jsong on 05/03/2017.
+ * <p>
+ * hackerrank	: https://www.hackerrank.com/jsong00505
+ * github		: https://github.com/jsong00505
+ * linkedin		: https://www.linkedin.com/in/junesongskorea/
+ * email		: jsong00505@gmail.com
+ * <p>
+ * challenge	: Java Loops I
+ */
+public class Solution {
+	public static void main(String[] args) {
+		try (
+			Scanner in = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(System.out);
+		) {
+			// get an integer N
+			int N = in.nextInt();
+
+			// validation
+			if(N < 2 || N > 20) {
+				throw new Exception("N is out of range.");
+			}
+
+			// iterating
+			for(int i = 1; i <= 10; i++) {
+				out.printf( "%d x %d = %d\n", N, i, N * i);
+			}
+		} catch(Exception e) {
+
+		}
+	}
+}
