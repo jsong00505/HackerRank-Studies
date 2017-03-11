0eaf570f1d33c60592ee003366d26a9c7cc34370
jsong00505 <jsong00505@gmail.com>
2017-03-11 14:35:05 +0900

    chore: indentation fixed

--- java/src/strings/regex/Solution.java
+++ java/src/strings/regex/Solution.java
@@ -28,12 +28,12 @@ class MyRegex {
 
 	MyRegex() {
 		/*
-		 * ? >> once or not at all
-		 * | >> The choice (also known as alternation or set union) operator matches either the expression before
-		 * 		or the expression after the operator
-		 * () >> The string matched within the parentheses can be recalled later
-		 * [] >> Matches a single character that is contained within the brackets
-		 * {m,n} >> 	Matches the preceding element at least m and not more than n times.
+		 * ?		>> once or not at all
+		 * |		>> The choice (also known as alternation or set union) operator matches either the expression before
+		 * 			or the expression after the operator
+		 * ()		>> The string matched within the parentheses can be recalled later
+		 * []		>> Matches a single character that is contained within the brackets
+		 * {m,n}	>> Matches the preceding element at least m and not more than n times.
 		 */
 
 		String IPClass = "([01]?[0-9]{0,2}|2([0-4]?[0-9]?|5[0-5]))";
