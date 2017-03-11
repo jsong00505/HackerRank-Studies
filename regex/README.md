352c3f8803d77f380579c80bd56ac1dad907b206
jsong00505 <jsong00505@gmail.com>
2017-03-11 14:35:51 +0900

    feat: Matching Anything But a Newline

--- regex/README.md
+++ regex/README.md
@@ -3,3 +3,5 @@
 | Domain    | Day | Difficulty |Challenge | Solution |
 | --------- | --- | ---------- | -------- | -------- |
 | RegEx     | 1   | Easy       | [Matching Specific String](https://www.hackerrank.com/challenges/matching-specific-string) | [Solution.java](src/introduction/specificstring/Solution.java) |
+| RegEx     | 1   | Easy       | [Matching Anything But a Newline](https://www.hackerrank.com/challenges/matching-anything-but-new-line) | [Solution.java](src/introduction/anything/Solution.java) |
+
---
+++ regex/src/anything/Solution.java
@@ -0,0 +1,45 @@
+package anything;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jsong on 11/03/2017.
+ * <p>
+ * hackerrank	: https://www.hackerrank.com/jsong00505
+ * github		: https://github.com/jsong00505
+ * linkedin		: https://www.linkedin.com/in/junesongskorea/
+ * email		: jsong00505@gmail.com
+ * <p>
+ * challenge	: Matching Anything But a Newline
+ */
+public class Solution {
+	public static void main(String[] args) {
+		// declare Tester class
+		Tester tester = new Tester();
+
+		/*
+		 * check the pattern matched
+		 * .	>> anything
+		 * {n}	>> Matches the preceding element n times.
+		 * \\.	>> A slash(\) means escape characters(here, dot)
+		 */
+		tester.check(".{3}\\..{3}\\..{3}\\..{3}");
+
+	}
+}
+
+class Tester {
+	public void check(String pattern){
+		Scanner scanner = new Scanner(System.in);
+		String testString = scanner.nextLine();
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(testString);
+		boolean match = m.matches();
+		System.out.format("%s", match);
+
+		//I'd love to close Scanner.
+		scanner.close();
+	}
+}
