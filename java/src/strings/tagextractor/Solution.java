325ea1a0880aefed27c56456733d374fc4dc3186
jsong00505 <jsong00505@gmail.com>
2017-03-20 09:25:22 +0900

    feat: Tag Content Extractor

--- java/src/strings/tagextractor/Solution.java
+++ java/src/strings/tagextractor/Solution.java
@@ -1,5 +1,6 @@
 package strings.tagextractor;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,36 +17,48 @@ import java.util.regex.Pattern;
  */
 public class Solution {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int testCases = Integer.parseInt(in.nextLine());
+		try(
+			Scanner in = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(System.out);
+		) {
+			// get the number of test cases
+			int testCases = Integer.parseInt(in.nextLine());
 
-		// constraints
-		assert(testCases < 1 || testCases > 100);
+			// constraints
+			assert(testCases < 1 || testCases > 100);
 
-		while(testCases > 0){
-			String line = in.nextLine();
+			while(testCases > 0){
+				String line = in.nextLine();
 
-			// regex
-			/*
-4
-<h1>Nayeem loves counseling</h1>
-<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
-<Amee>safat codes like a ninja</amee>
-<SA premium>Imtiaz has a secret crush</SA premium>
-			*/
+				/**
+				 * (.+)		>> any characters but it should matches once
+				 * ([^<]+)	>> like above but not include <(bracket)
+				 * \1		>> capture first group(very first HTML start and end tag here>
+				 **/
+				String regEx = "<(.+)>([^<]+)</\\1>";
+				Pattern p = Pattern.compile(regEx);
+				Matcher m = p.matcher(line);
 
-			String regEx = "(<\\b.+\\b>)\\1";
-			Pattern p = Pattern.compile(regEx);
-			Matcher m = p.matcher(line);
+				// just check if not match
+				boolean matchFlag = false;
 
-			while(m.find()) {
-				System.out.println(m.group());
+				while(m.find()) {
+					// group #2 is the content between HTML start and end tag
+					if(m.groupCount() == 2) {
+						out.println(m.group(2));
+					}
+
+					matchFlag = true;
+				}
+
+				if(!matchFlag) {
+					out.println("None");
+				}
+
+				testCases--;
 			}
-
-			testCases--;
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-
-		// close Scanner
-		in.close();
 	}
 }
